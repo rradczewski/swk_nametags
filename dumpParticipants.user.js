86e163004601856d43dad88d05d19a00c02594fe
Raimo Radczewski <raimo.radczewski@vaamo.de>
2015-03-18 11:29:06 +0100

    Add UserScript for dumping participants into a json file

---
+++ dumpParticipants.user.js
@@ -0,0 +1,21 @@
+(function() {
+var members = [];
+$.ajaxSetup({async: false});
+$(".member a:first-child").each(function(i, link) { 
+	$.get(link.href, function(userHtml) { 
+		var dom = $(userHtml);
+		var user = {};
+		dom.find("p").each(function(i, el) {
+			var parts = el.outerText.split(/:\s/);
+			if(parts.length < 2) return;
+			user[parts[0]] = parts[1];
+		});
+		members.push(user);
+	});
+});
+console.log("Downloading Infos for "+members.length+" participants");
+var a = document.createElement("a");
+a.download = "participants.json";
+a.href = "data:application/json;base64,"+btoa(JSON.stringify(members));
+a.click();
+})();
